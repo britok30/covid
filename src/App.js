73763d06b749b148411f3d90aec7201b733067de
Kelvin Brito <britok30@gmail.com>
2020-06-19 00:45:46 -0400

    hero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="hero">
+                <header className="hero-text">
+                    <h1 className="hero-lead">Corona</h1>
+                    <h2 className="hero-sublead">Viruse Disease (COVID-19)</h2>
+                    <p className="hero-p">
+                        Pray for the coronavirus victims. This virus was first
+                        reported in Wuhan, Hubai, China on November 17th, 2019 &
+                        on March, 11th, 2020, the World Health Organization
+                        (WHO) declared the outbreak a pandemic.
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
